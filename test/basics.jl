b18aa3a158930a3cec363d7987938d3375e4bff9
UnofficialJuliaMirrorBot <43731649+UnofficialJuliaMirrorBot@users.noreply.github.com>
2019-05-23 08:21:16 +0000

    Snapshot of branch master taken on 2019-05-23T04:21:13.706-04:00 from "https://github.com/UnofficialJuliaMirror/CSV.jl-336ed68f-0bac-5ca0-87d4-7b16caf5d00b"

--- test/basics.jl
+++ test/basics.jl
@@ -307,4 +307,15 @@ row = first(f)
 row = iterate(f, 2)[1]
 @test row.int_float === 3.14
 
+# reported by oxinabox on slack; issue w/ manually specified pool column type and 0 rows
+df = CSV.read(IOBuffer("x\n"), types=[CSV.PooledString], copycols=true)
+@test size(df) == (0, 1)
+
+df = CSV.read(IOBuffer("x\n"), types=[Union{CSV.PooledString, Missing}], copycols=true)
+@test size(df) == (0, 1)
+
+f = CSV.File(IOBuffer("x\n1\n2\n3\n#4"), comment="#")
+@test length(f.x) == 3
+@test f.x[end] == 3
+
 end
